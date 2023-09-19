2225d3acbd4ca6b19b4c318e71abc32ef141f391
John Schachte <johnpete@ad.unc.edu>
2023-09-18 22:53:08 -0400

    deleted ManagementEmail.js

--- Test/testManagementEmail.js
+++ Test/testManagementEmail.js
@@ -1,52 +1,17 @@
 class TestManagementEmails extends TestCoachingRow{
     constructor() {
         super(); // Call the parent class constructor
-        // this.noCoaching = [];
-        // this.needCoaching = [];
-        // this.coachingHeaders = {
-        //     "Request Id" : 0,
-        //     "Timestamp" : 1,
-        //     "Agent's Name" : 2,
-        //     "Supervisor" : 3,
-        //     "Email Address" : 4,
-        //     "Coaching Identifier?" : 5,
-        //     "Ticket Link" :6,
-        //     "Severity?":7,
-        //     "Category?":8,
-        //     "Describe?":9
-        // }
     }
 
-    runCoachingFormatTest() {
+    runTestManagementEmail(howMany = -10) {
         this.filterForCoachings();
-        const formattedRows = this.formatCoachingRows();
-        const last10 = formattedRows.slice(-10);
-        last10.forEach((row, index) => {
-            sendManagementCoachingEmail(...row);
+        this.formattedRows = this.formattedRows ? this.formattedRows : this.formatCoachingRows();
+        const lastHowMany = formattedRows.slice(howMany);
+        const sendManagementCoachingEmailBound = sendManagementCoachingEmail.bind(this)
+        lastHowMany.forEach((row, index) => {
+            sendManagementCoachingEmailBound(row.coachingRow,row.agentObject);
         });
     }
-    // filterForCoachings(){
-    //     this.testRows.forEach((row,i) => {
-    //         // Logger.log(i);
-    //         let updateValues = new Array(this.colMap.size);
-    //         let score = row[this.colMap.get(SCORE_HEADER)];
-    //         // Logger.log("score before transform: %s",score);
-    //         score = updateScoreValues(updateValues,this.colMap,score)/10000;
-    //         // Logger.log("score after transform: %s",score);
-
-    //         if(!score) return;
-    //         let {severity,categories} = determineCoachingNeed(row,this.colMap,score);
-    //         if(severity){
-    //             const agentObj = NameToWFM.getAgentObj(row[this.colMap.get(AGENT_NAME_HEADER)]);
-    //             if(!agentObj){
-    //               Logger.log("%s produced a null agentObj. Please research.",row[this.colMap.get(AGENT_NAME_HEADER)]); // there were 4 and these employees were found on former employee sheet
-    //               return;
-    //             }
-    //             this.needCoaching.push({row,agentObj,severity,categories,score});
-    //         }
-    //     });
-    //     Logger.log("Need Coaching: %s",this.needCoaching);
-    // }
     formatCoachingRows() {
         const formatAsCoachingRowBinded = formatAsCoachingRow.bind(this)
         return this.needCoaching.map(el => {
@@ -55,6 +20,35 @@ class TestManagementEmails extends TestCoachingRow{
     }
 }
 
-function testFormatRow(){
-  new TestManagementEmails().runCoachingFormatTest();
+function testSendManagementEmail(){
+  new TestManagementEmails().runTestManagementEmail();
+}
+
+function checkEmailQuota() {
+  var remainingQuota = MailApp.getRemainingDailyQuota();
+  Logger.log('Remaining email quota: ' + remainingQuota);
+}
+
+class TestHttpRequest extends TestCoachingRow{
+    constructor(){
+        super();
+    }
+
+    runTestHttpRequest(howMany = -10){
+        this.filterForCoachings();
+        this.formattedRows = this.formattedRows ? this.formattedRows : this.formatCoachingRows();
+        const lastHowMany = this.formattedRows.slice(howMany);
+
+        lastHowMany.forEach((row, index) => {
+            const requestOptions = {
+                method: 'post',
+                contentType: 'application/json',
+                headers: {
+                    Authorization: 'Bearer ' + CoachingRequestScripts.getOAuthService().getAccessToken()
+                }
+            };
+            requestOptions["payload"] = JSON.stringify(row.coachingRow); // prepare for request
+            sendHttpWIthRetry("https://script.google.com/a/macros/shift4.com/s/AKfycbzVwcCdBlPVyTrjXjd0aPTf_iWYe9tJLCTPhUHGqA7FQ-ownSx0ZIKz6Ovkgl_WQw8lTA/exec",this.requestOptions);
+        });
+    }
 }
