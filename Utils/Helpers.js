56ccdf2a5f36160811da818777c5895f757f7b4c
John Schachte <johnpete@ad.unc.edu>
2023-09-19 09:32:41 -0400

    missed saving headers

--- Utils/Helpers.js
+++ Utils/Helpers.js
@@ -4,41 +4,41 @@ function getColMap(){
     return Custom_Utilities.mkColMap(memoizedReads(BACKEND_ID,`${SUBMISSION_SHEET_NAME}!1:1`,{majorDimension:"ROWS"}).values[0]);
 }
 
-  /**
-   * logic for transforming transcriptIds.
-   * @param {String[]} transcriptIds - an array of transcript ids to transform or null.
-   * @return {Array} - An array of transformed transcript ids.
-   */
-  function transformTranscriptIds(transcriptIds){
-    if(!transcriptIds){
-      return false;
-    }
-    return transcriptIds.split(/\s*\,\s*/g).map(id =>({href:"https://na1.nice-incontact.com/player/#/cxone-player/segments/" + id, id}));
+/**
+ * logic for transforming transcriptIds.
+ * @param {String[]} transcriptIds - an array of transcript ids to transform or null.
+ * @return {Array} - An array of transformed transcript ids.
+ */
+function transformTranscriptIds(transcriptIds){
+  if(!transcriptIds){
+    return false;
   }
+  return transcriptIds.split(/\s*\,\s*/g).map(id =>({href:"https://na1.nice-incontact.com/player/#/cxone-player/segments/" + id, id}));
+}
 
-  function retry(tryFunct, waitTime = 1000,maxRetries=Number.MAX_VALUE) {
-    let result,tries=0; // the thing we're trying to get
-    while(waitTime > 0 && tries < maxRetries){
-      try{
-        result = tryFunct();
-        waitTime = 0;
-      }catch(L){
-        Logger.log(L);
-        Custom_Utilities.longSleep(waitTime);
-        tries++;
-      }
+function retry(tryFunct, waitTime = 1000,maxRetries=Number.MAX_VALUE) {
+  let result,tries=0; // the thing we're trying to get
+  while(waitTime > 0 && tries < maxRetries){
+    try{
+      result = tryFunct();
+      waitTime = 0;
+    }catch(L){
+      Logger.log(L);
+      Custom_Utilities.longSleep(waitTime);
+      tries++;
     }
-    return result;
   }
+  return result;
+}
 
-  function sendEmail(recipients,subject,template){
-    Logger.log("receipients: %s",recipients);
-    // CHANGE FOR PRODUCTION!!! THIS IS THE TEST ENDPOINT.
-    GmailApp.sendEmail("jschachte@shift4.com",subject,"",{
-    // GmailApp.sendEmail(recipients,subject,"",{
-      htmlBody: template.evaluate().getContent()
-    });
-  }
+function sendEmail(recipients,subject,template){
+  Logger.log("receipients: %s",recipients);
+  // CHANGE FOR PRODUCTION!!! THIS IS THE TEST ENDPOINT.
+  GmailApp.sendEmail("jschachte@shift4.com",subject,"",{
+  // GmailApp.sendEmail(recipients,subject,"",{
+    htmlBody: template.evaluate().getContent()
+  });
+}
 
 function setScoreFormat(startRow,colMap){
   const ss = SpreadsheetApp.openById(BACKEND_ID);
