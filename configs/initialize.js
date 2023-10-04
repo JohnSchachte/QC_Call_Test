5b5d3f6dd1e56545ea52f9b3c13007405a6521a9
John Schachte <johnpete@ad.unc.edu>
2023-10-04 10:37:08 -0400

    changed initialization

--- configs/initialize.js
+++ configs/initialize.js
@@ -1,26 +1,25 @@
 function initializeLastRow() {
   // const submSheet = SpreadsheetApp.openById(BACKEND_ID).getSheetByName("Submissions");
-  scriptProps.setProperty("lr",(4471).toString()); // change number to the last row you want to do. Test row = 4347
+  scriptProps.setProperty("lr",(4541).toString()); // change number to the last row you want to do. 
 }
 
 // /**
 //  * row 1576 shows an example of the race condition of using a form submit with a getLastRow() 
 //  */
 
-// function initializeFormTrigger(){
-//   const form = FormApp.openById("17sSSqjmpEeb1an8KtRYqEP29ms7FhAE-oMlqteGkFU0")
-//   ScriptApp.newTrigger("mainWrapper")
-//     .forForm(form)
-//     .onFormSubmit()
-//     .create();
-// }
+function initializeFormTrigger(){
+  const form = FormApp.openById("1imwmnGELgUAelDO1p6qRbXF5uw3AaP1_dyKj5xO5qkw")
+  ScriptApp.newTrigger("mainWrapper")
+    .forForm(form)
+    .onFormSubmit()
+    .create();
+}
 
 function setScriptProperties(){
   
   // if in production, this should be true
-  scriptProps.setProperty("IS_PRODUCTION","false");
+  scriptProps.setProperty("IS_PRODUCTION","true");
   scriptProps.setProperty("IS_CALL","false");
-  
   // this will depend on chat or call schema/headers
   scriptProps.setProperty("DATE_SENT_HEADER","Date Sent");
   scriptProps.setProperty("TRANSCRIPT_ID_HEADER","Chat Transcript Segment ID#");
@@ -46,17 +45,16 @@ function setScriptProperties(){
   // criteria fields/headers
   scriptProps.setProperty("SECURITY_HEADER", "Did the agent adhere to all account security guidelines while assisting the merchant?");
   scriptProps.setProperty("FILED_TICKET_HEADER","Did the agent file/document a ticket to record this interaction with the chatter?");
-  scriptProps.setProperty("WORK_AVOIDANCE_HEADER","Did the agent work efficiently? "); // reliability wants this criteria not to count
+  scriptProps.setProperty("WORK_AVOIDANCE_HEADER","Did the agent work efficiently? ");
   scriptProps.setProperty("EVALUATOR_HEADER","Evaluator Name");
   scriptProps.setProperty("MID_DBA_HEADER","MID & DBA Name");
   
   // All these need to be granted permissions if you want to run the script
   scriptProps.setProperty("Response Sheet Name","Chat Form Responses");
-  scriptProps.setProperty("BACKEND_ID","1SY1_eBIKx7bpIv7zOpSgm1v9rIqCyCjSZzeReysqfic");
-  // coaching reporting
+  scriptProps.setProperty("BACKEND_ID","1a36JwaGQAgUWs-Pa6dkM6XfmiaJVyCiXlt9vZeLL6e4");
   scriptProps.setProperty("REPORTING_ID","1zQ98-rxOzfeq1QOmVeaC7OgOcZ0IaD7vv7_Vak7rclE");
   // reliability coaching reporting
-  scriptProps.setProperty("REPORTING_SS_ID","1QSTxbx-HfTZbQpDqaAYuj9T7784HWJ7ABQ4BsT_Ql_U");
+  scriptProps.setProperty("REPORTING_SS_ID","1-h8et_sdVVSmUbKq-ZK86-K93VRyX80xW2r2fgmIIhQ");
   
   scriptProps.setProperty("REPORTING_SHEET_NAME","Coaching_Form_Data");
 }
