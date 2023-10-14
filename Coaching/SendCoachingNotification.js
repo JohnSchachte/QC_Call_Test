78a24c8654afe7e2e341bf93e4602a87bc9a36c4
John Schachte <johnpete@ad.unc.edu>
2023-10-14 12:44:47 -0400

    manually fixing snedcoachingnotfication

--- Coaching/SendCoachingNotification.js
+++ Coaching/SendCoachingNotification.js
@@ -7,47 +7,48 @@
  * @returns {void}
  */
 function sendManagementCoachingEmail(coachingRow, agentObject, coachingId = "test22") {
-  if (!agentObject) {
-      return;
+    if (!agentObject) {
+        return;
+    }
+    try {
+      CoachingRequestScripts.getEmails(agentObj);
+    } catch (f) {
+        Logger.log(f);
+        writeCoachingStatus(a1Notation, "No Sup or Manager. Tried at " + new Date().toLocaleString());
+        return;
+    }
+    const cache = CacheService.getScriptCache();
+    const getTeams = Custom_Utilities.memoize(() => CoachingRequestScripts.getTeams(REPORTING_ID), cache);
+  
+    // Try to get the coaching ID anchor, falling back to backend if necessary
+    let coachingIdAnchor;
+    const teams = getTeams();
+    try {
+        coachingIdAnchor = CoachingRequestScripts.getSupCoachingSheet(teams, agentObject["Team"], agentObject["SUPERVISOR"]);
+    } catch (f) {
+        Logger.log("trying to get sup Sheet. Error: %s", f);
+        coachingIdAnchor = CoachingRequestScripts.getBackEndNew(teams, agentObject["Team"])
+    }
+  
+    // Populate email variables based on coaching details
+    const emailVars = {
+        agentName: agentObject["Employee Name"],
+        coachingId,
+        supSheet: coachingIdAnchor,
+        transcriptIds: transformTranscriptIds(coachingRow[this.coachingHeaders["Coaching Identifier?"]]),
+        ticket: coachingRow[this.coachingHeaders["Ticket Link"]] == "No Ticket" ?
+            [coachingRow[this.coachingHeaders["Ticket Link"]]] :
+            coachingRow[this.coachingHeaders["Ticket Link"]].match(/\d{7}/g).map(el => { return { "id": el, "url": "https://tickets.shift4.com/#/tickets/" + el } }),
+        severity: coachingRow[this.coachingHeaders["Severity?"]],
+        reason: coachingRow[this.coachingHeaders["Category?"]],
+        description: coachingRow[this.coachingHeaders["Describe?"]],
+        agentEmail: agentObject["Email Address"]
+    };
+  
+    // Prepare and send the email with the specified template
+    const template = HtmlService.createTemplateFromFile("HTML/agent_coaching");
+    template.vars = emailVars;
+  
+    sendEmail(CoachingRequestScripts.getEmails(agentObject), "Evaluation Coaching: " + agentObject["Employee Name"], template);
   }
-  try {
-    CoachingRequestScripts.getEmails(agentObj);
-  } catch (f) {
-      Logger.log(f);
-      writeCoachingStatus(a1Notation, "No Sup or Manager. Tried at " + new Date().toLocaleString());
-      return;
-  }
-  const cache = CacheService.getScriptCache();
-  const getTeams = Custom_Utilities.memoize(() => CoachingRequestScripts.getTeams(REPORTING_ID), cache);
-
-  // Try to get the coaching ID anchor, falling back to backend if necessary
-  let coachingIdAnchor;
-  const teams = getTeams();
-  try {
-      coachingIdAnchor = CoachingRequestScripts.getSupCoachingSheet(teams, agentObject["Team"], agentObject["SUPERVISOR"]);
-  } catch (f) {
-      Logger.log("trying to get sup Sheet. Error: %s", f);
-      coachingIdAnchor = CoachingRequestScripts.getBackEndNew(teams, agentObject["Team"])
-  }
-
-  // Populate email variables based on coaching details
-  const emailVars = {
-      agentName: agentObject["Employee Name"],
-      coachingId,
-      supSheet: coachingIdAnchor,
-      transcriptIds: transformTranscriptIds(coachingRow[this.coachingHeaders["Coaching Identifier?"]]),
-      ticket: coachingRow[this.coachingHeaders["Ticket Link"]] == "No Ticket" ?
-          [coachingRow[this.coachingHeaders["Ticket Link"]]] :
-          coachingRow[this.coachingHeaders["Ticket Link"]].match(/\d{7}/g).map(el => { return { "id": el, "url": "https://tickets.shift4.com/#/tickets/" + el } }),
-      severity: coachingRow[this.coachingHeaders["Severity?"]],
-      reason: coachingRow[this.coachingHeaders["Category?"]],
-      description: coachingRow[this.coachingHeaders["Describe?"]],
-      agentEmail: agentObject["Email Address"]
-  };
-
-  // Prepare and send the email with the specified template
-  const template = HtmlService.createTemplateFromFile("HTML/agent_coaching");
-  template.vars = emailVars;
-
-  sendEmail(CoachingRequestScripts.getEmails(agentObject), "Evaluation Coaching: " + agentObject["Employee Name"], template);
-}
+  
